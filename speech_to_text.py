f6e65d700f7605bd20086dc02dd1903b4526a4d8
Valentyner <robertdoms2002@gmail.com>
2024-05-03 03:45:06 +0300

    The beggining

---
+++ speech_to_text.py
@@ -0,0 +1,20 @@
+import whisper
+
+model = whisper.load_model("base")
+
+audio = whisper.load_audio("audio_2024-05-03_02-40-36.ogg")
+audio = whisper.pad_or_trim(audio)
+
+# make log-Mel spectrogram and move to the same device as the model
+mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+# detect the spoken language
+_, probs = model.detect_language(mel)
+print(f"Detected language: {max(probs, key=probs.get)}")
+
+# decode the audio
+options = whisper.DecodingOptions()
+result = whisper.decode(model, mel, options)
+
+# print the recognized text
+print(result.text)
