209751de9b426b8b0698bea1fd3b6a3d70ef5b48
Valentyner <robertdoms2002@gmail.com>
2024-05-07 10:09:07 +0300

    gTTS added

--- text2speech.py
+++ text2speech.py
@@ -1,5 +1,7 @@
 import os
 
+from gtts import gTTS
+
 from deepgram import (
     DeepgramClient,
     SpeakOptions,
@@ -10,16 +12,18 @@ filename = "output2.wav"
 
 def text2speech(text):
     try:
-        SPEAK_OPTIONS = {"text": text}
-        deepgram = DeepgramClient(api_key="2eb3bb5301dc61e51e3b5d9d6722eaf9dc443625")
-
-        options = SpeakOptions(
-            model="aura-asteria-en",
-            encoding="linear16",
-            container="wav"
-        )
-
-        response = deepgram.speak.v("1").save(filename, SPEAK_OPTIONS, options)
+        # SPEAK_OPTIONS = {"text": text}
+        # deepgram = DeepgramClient(api_key="2eb3bb5301dc61e51e3b5d9d6722eaf9dc443625")
+        #
+        # options = SpeakOptions(
+        #     model="aura-asteria-en",
+        #     encoding="linear16",
+        #     container="wav"
+        # )
+        #
+        # response = deepgram.speak.v("1").save(filename, SPEAK_OPTIONS, options)
+        tts = gTTS(text=text, lang='uk')
+        tts.save(filename)
         return filename
 
     except Exception as e:
