e3ada9abd8fdf2e99d27f5933b9055f9d8d2df86
Valentyner <robertdoms2002@gmail.com>
2024-05-07 09:58:14 +0300

    groq_service complited

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, send_file, render_template
 import tempfile
 from text2speech import text2speech
 from speech2text import speech2text
+from groq_service import execute
 
 app = Flask(__name__)
 
@@ -9,6 +10,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/process-audio", methods=["POST"])
 def process_audio():
@@ -18,8 +20,10 @@ def process_audio():
         temp_audio.write(audio_data)
         temp_audio.flush()
 
-    text = speech2text((temp_audio.name))
-    generated_speech = text2speech(text)
+    text = speech2text(temp_audio.name)
+    generated_answer = execute(f"Будь ласка дай відповідь на запит Українською мовою {text}")
+    print(generated_answer)
+    generated_speech = text2speech(generated_answer)
 
     return send_file(generated_speech, mimetype="audio/mpeg")
 
